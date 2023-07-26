85fd20ea2cc428a4da6782e797002361e2ee4a78
Taira Yuji <e23008@std.it-college.ac.jp>
2023-07-26 20:24:41 +0900

    07/26 start

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 const Home = () => {
   return (
     <>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
+      <header>HEADER</header>
+      <main>
+        <div>
+        <h1>CUBE</h1>
+        <hr />
+        <p>アウトプットしていくサイト</p>
+      </div>
+      </main>
+      <footer>FOOTER</footer>
     </>
-
   )
 }
-
 export default Home
