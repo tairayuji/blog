f23f8818cae0a93c5d003b0da803ecf7e65d21ef
Taira Yuji <e23008@std.it-college.ac.jp>
2023-08-09 18:46:15 +0900

    commit 2.9

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,6 @@
 import Hero from 'components/hero'
 
 const Blog = () => {
-  return (
-    <Hero
-      title='Blog'
-      subtitle='Recent Posts'
-    />
-  )
+  return <Hero title='Blog' subtitle='Recent Posts' />
 }
 export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 import Hero from 'components/hero'
 
 const Home = () => {
-  return (
-    <Hero
-      title='CUBE'
-      subtitle='アウトプットしていくサイト'
-      imageOn
-    />
-  )
+  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
 }
 export default Home
